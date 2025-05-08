5eaa5ad0fd3c54ca091211da25a08e1bb1261d68
Arthur <arthursimonin88@gmail.com>
2025-05-08 15:18:59 +0200

    fix: getMatchData issue from scrapping or database

--- api.py
+++ api.py
@@ -34,20 +34,36 @@ def home():
 
 @app.get("/match/{match_id}")
 def get_match_data(match_id: int):
+    from scraper.match import get_match_data as scrape_match_data
     db = SessionLocal()
     match = db.query(MatchDetails).filter(MatchDetails.match_id == match_id).first()
-    db.close()
 
     if not match:
-
-        from background import store_match_details
-        match = store_match_details(match_id=match_id)
-
-        if not match:
+        try:
+            print(f"⏳ Scraping match {match_id}...")
+            details = scrape_match_data(match_id)
+            print(f"✅ Scraping terminé.")
+            match = MatchDetails(
+                match_id=match_id,
+                team_1=details["team_1"],
+                team_2=details["team_2"],
+                team_1_score=details["team_1_score"],
+                team_2_score=details["team_2_score"],
+                score_named_with_dash=details["score_named_with_dash"],
+                score_with_dash=details["score_with_dash"],
+                score_named_with_colon=details["score_named_with_colon"],
+                score_with_colon=details["score_with_colon"],
+                games=details["games"]
+            )
+            db.add(match)
+            db.commit()
+        except Exception as e:
             db.close()
-            return{"error": "Unable to retrieve match details."}
-
-    return {
+            return {"error": f"Scraping échoué pour match {match_id}: {str(e)}"}
+    else:
+        print(f"✅ Match {match_id} trouvé en base de données.")
+        
+    result = {
         "match_id": match.match_id,
         "team_1": match.team_1,
         "team_2": match.team_2,
@@ -59,6 +75,9 @@ def get_match_data(match_id: int):
         "score_with_colon": match.score_with_colon,
         "games": match.games
     }
+    db.close()
+    return result
+
 
 @app.get("/matchs/results")
 def get_match_results(limit: int = None):
