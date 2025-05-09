3d908723f4f02a17b81e51df59157c8eec73b7ad
Arthur <arthursimonin88@gmail.com>
2025-05-09 18:28:50 +0200

    fix: update method name

--- app/routes/match_routes.py
+++ app/routes/match_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.controllers.match_controller import fetch_live_matches
+from app.controllers.match_controller import fetch_match_from_id
 
 router = APIRouter(prefix="/matches", tags=["matches"])
 
 @router.get("/match/{match_id}")
 def get_match_from_id(match_id: int):
-    return fetch_live_matches(match_id) 
+    return fetch_match_from_id(match_id) 
