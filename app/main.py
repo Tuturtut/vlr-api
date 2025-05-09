3a1a0770485693a968a6f3d4eb5a48fcf1dbb26e
Arthur <arthursimonin88@gmail.com>
2025-05-09 18:29:34 +0200

    feat: Add default route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from app.routes import match_routes  # fichier à venir
+from app.routes import match_routes
 
 app = FastAPI()
 
 app.include_router(match_routes.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Valorant Live Scores API!"}
